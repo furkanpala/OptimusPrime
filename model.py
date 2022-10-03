f36f4cde577f2db432b32fe6814060f649d79914
furkanpala <furkan2pala@gmail.com>
2022-10-03 21:17:44 +0300

    Boilerplate self-attention class

---
+++ model.py
@@ -0,0 +1,17 @@
+import torch.nn as nn
+
+
+class SelfAttention(nn.Module):
+    def __init__(self) -> None:
+        super().__init__()
+
+    def forward():
+        pass
+
+
+def test():
+    pass
+
+
+if __name__ == "__main__":
+    test()
